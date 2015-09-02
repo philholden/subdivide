b35a5665445c3dd55f3a88dd8eb8e58353e6f16b
Phil Holden <philip.holden@ncr.com>
2015-09-02 10:51:48 +0100

    adjacent dragging

---
+++ src/actions/LayoutActions.js
@@ -0,0 +1,38 @@
+import {
+  SET_SPLIT_RATIO,
+  SPLIT,
+  JOIN,
+  SET_SIZE
+} from '../constants/BlenderLayoutConstants';
+
+export function setSplitRatio(id, splitRatio) {
+  return {
+    type: SET_SPLIT_RATIO,
+    splitRatio,
+    id
+  };
+}
+
+export function split(id, splitType){
+  return {
+    type: SPLIT,
+    id,
+    splitType
+  };
+}
+
+export function join(retainId, removeId){
+  return {
+    type: JOIN,
+    retainId,
+    removeId
+  };
+}
+
+export function setSize(width, height){
+  return {
+    type: SET_SIZE,
+    width,
+    height
+  };
+}
