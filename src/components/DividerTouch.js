8cc82b5165da44575047ce0aceed9ac25ed0eef3
Phil Holden <philip.holden@ncr.com>
2015-09-15 15:03:20 +0100

    dividers work properly

---
+++ src/components/DividerTouch.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import {
+  COL
+} from '../constants/BlenderLayoutConstants';
+
+export default class Divider extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.removeListeners = () => {
+      document.removeEventListener('mouseup', this.onMouseUp);
+    };
+
+    this.onMouseUp = () => {
+      const {actions} = this.props;
+      actions.setDividerDown(undefined);
+      this.removeListeners();
+    };
+
+    this.onMouseDown = ({clientX, clientY}) => {
+      const {actions, divider} = this.props;
+
+      actions.setDividerDown({...divider.toJS(), startX: clientX, startY: clientY});
+
+      document.addEventListener('mouseup', this.onMouseUp);
+    };
+  }
+
+  componentWillUnmount() {
+    this.removeListeners();
+  }
+
+  dividerStyle() {
+    const {width, height, top, left, direction} = this.props.divider;
+    const {touchMargin} = this.props.layout;
+    var touch = {
+      width,
+      height,
+      top,
+      left,
+ //     backgroundColor: 'rgba(0,0,0,0.5)',
+      position: 'absolute'
+    };
+
+    if (direction === COL) {
+      touch.cursor = 'row-resize';
+      touch.top -= touchMargin;
+      touch.height += touchMargin * 2;
+    } else {
+      touch.cursor = 'col-resize';
+      touch.left -= touchMargin;
+      touch.width += touchMargin * 2;
+    }
+
+    return {touch};
+  }
+
+  render() {
+
+    const styles = this.dividerStyle();
+
+    return (
+      <div
+        style={styles.touch}
+        onMouseDown={this.onMouseDown}
+        className="divider">
+        <div style={styles.border}>
+           <div style={styles.inner}></div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/Dividers.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import {
+  COL
+} from '../constants/BlenderLayoutConstants';
+import DividerTouch from './DividerTouch';
+
+var Rect = (props) => {
+  let {style} = props;
+  const {left, top} = props.style;
+  style = {
+    ...{},
+    ...style,
+    transform: `translate3d(${left}px,${top}px,0)`,
+    position: 'absolute',
+    top: 0,
+    left: 0
+  };
+  return <div {...props} style={style}></div>;
+};
+
+var Dividers = (props) => {
+  const {dividers, layout, actions} = props;
+  const {borderSize} = layout;
+  //let touch = dividers.map(touch).toSeq();
+  let toBorder = (divider) => {
+    const {width, height, top, left, id} = divider;
+    let style = {
+      width,
+      height,
+      top,
+      left,
+      backgroundColor: '#c0c0d0'
+    };
+
+    return <Rect style={style} key={id} />;
+  };
+
+  let toInner = (divider) => {
+    const {width, height, top, left, id, direction} = divider;
+    let style;
+    if (direction === COL) {
+      style = {
+        width: width + borderSize * 2,
+        height: height - borderSize * 2,
+        top: top + borderSize,
+        left: left - borderSize,
+        backgroundColor: '#e0e0f0'
+      };
+    } else {
+      style = {
+        width: width - borderSize * 2,
+        height: height + borderSize * 2,
+        top: top - borderSize,
+        left: left + borderSize,
+        backgroundColor: '#e0e0f0'
+      };
+    }
+
+    if (style.left < 0) {
+      style.width = style.width + style.left;
+      style.left = 0;
+    }
+    style.width = Math.min(style.width, layout.width - style.left);
+    if (style.top < 0) {
+      style.height = style.height + style.top;
+      style.top = 0;
+    }
+    style.height = Math.min(style.height, layout.height - style.top);
+
+    return <Rect style={style} key={id} />;
+  };
+
+  let toTouch = (divider) => {
+    return <DividerTouch
+              divider={divider}
+              layout={layout}
+              actions={actions}
+              key={divider.id}
+            />;
+  };
+
+  return (
+    <div>
+      {dividers.toList().map(toBorder)}
+      {dividers.toList().map(toInner)}
+      {dividers.toList().map(toTouch)}
+    </div>
+  );
+};
+
+export default Dividers;
